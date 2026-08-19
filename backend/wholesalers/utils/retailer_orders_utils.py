772cee06d5e1c12f36d0636ac15b26f54273ca77
emuswailit <emuswailit@gmail.com>
2026-08-19 09:09:19 +0300

    customer order payment

--- backend/wholesalers/utils/retailer_orders_utils.py
+++ backend/wholesalers/utils/retailer_orders_utils.py
@@ -1413,7 +1413,7 @@ def process_retailer_order_payment(data,user):
         payment_method = PaymentMethods.objects.filter(id=data['payment_method']).first()
     else:
         errors.append("No payment method with provided ID exists")
-
+    create_log("INFO", f"Payment method: { payment_method.title}")
     if payment_method.title =="CASH":
         # reference_number = generate_reference_number(retailer_order.wholesaler, user)
         retailer_order_payment = RetailerOrderPayments.objects.create(
