f88b9957452ad562d2e52a0a3dc0f6822dcfa8ca
emuswailit <emuswailit@gmail.com>
2026-09-19 06:20:53 +0300

    Backend fixes

--- backend/wholesalers/routing.py
+++ backend/wholesalers/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path,path
 from . import consumers
 
 wholesalers_websocket_urlpatterns = [
-    path("wholesalers/inventory/",consumers.WholesalerInventoryConsumer.as_asgi())
+    path("ws/wholesalers/inventory/",consumers.WholesalerInventoryConsumer.as_asgi())
 ]
