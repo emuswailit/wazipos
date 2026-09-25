d9b6dd27a9f5b6dc9586db0423fc8b08705d0925
emuswailit <emuswailit@gmail.com>
2026-09-25 15:18:14 +0300

    Rename category

--- backend/drugs/utils/generics_utils.py
+++ backend/drugs/utils/generics_utils.py
@@ -42,7 +42,7 @@ def validate_generic_data(data):
         if (
             title
             and Generics.objects.filter(
-                title=title.upper(), drug_class_id=drug_class_id
+                title=title.upper(), drug_class_id=drug_class
             ).exists()
         ):
             errors.append(
